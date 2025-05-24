a5f3d18ede75a07138bbb182fd5fb489372f7224
Arie Timmerman <arietimmerman@gmail.com>
2025-05-24 09:12:05 +0200

    cleansing

--- additionals.js
+++
@@ -1,4 +0,0 @@
-new Image().src='https://nullitics.com/null.gif'+
-'?u='+encodeURI(location.href)+
-'&r='+encodeURI(document.referrer)+
-'&d='+screen.width
--- webapp/react-util.ts
+++
@@ -1,6 +0,0 @@
-export function prevent<T extends { preventDefault(): void }, U>(func: (e: T) => U) {
-  return function (e: T) {
-    e.preventDefault()
-    func(e)
-  }
-}
